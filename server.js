273187a7d399c5e760f43ad87102b23badb5c57e
Huertz <104065687+Huertz@users.noreply.github.com>
2022-11-26 01:43:59 -0600

    Almost done need to fix a bug

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const routes = require('./routes');
 
-// import sequelize connection
+// Import sequelize connection
+const sequelize = require('./config/connection');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -10,9 +12,10 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
+// Sync sequelize models to the database, then turn on the server
 sequelize.sync ({ force: false }).then(() => {
   app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}!`);
   });
 });
+
